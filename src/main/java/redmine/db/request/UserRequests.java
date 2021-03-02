0c6ff8d5a97fe2fe9304f20f5dad512ac8a5b011
Ksenia_Proskurnyak <proskurnyak@i-teco.ru>
2021-03-02 21:08:13 +0300

    UI

--- src/main/java/redmine/db/request/UserRequests.java
+++ src/main/java/redmine/db/request/UserRequests.java
@@ -1,12 +1,37 @@
 package redmine.db.request;
 
+import redmine.managers.Manager;
 import redmine.model.user.User;
+
+import java.util.List;
+import java.util.Map;
 
 public class UserRequests {
 
     public static User createUser(User user){
+
+        String query = "INSERT INTO public.users " +
+                "(id, login, hashed_password, firstname, lastname, admin, status, language, type, " +
+                "mail_notification, salt) " +
+                "VALUES(DEFAULT,?,?,?,?,?,?,?,?,?,?) RETURNING id;";
+        List<Map<String, Object>> result = Manager.dbConnection.executePreparedQuery(query,
+                user.getLogin(),
+                user.getPassword(),
+                user.getFirstname(),
+                user.getLastname(),
+                user.getAdmin(),
+                user.getStatus(),
+                "ru",
+                "User",
+                "all",
+                user.getSalt()
+        );
+        user.setId((Integer) result.get(0).get("id"));
+        return user;
+
+
         //запрос в БД
         //установка id значением, сгенерированным БД
-        return user;
+
     }
 }
---
+++ src/main/java/redmine/model/user/User.java
@@ -0,0 +1,46 @@
+package redmine.model.user;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import redmine.db.request.UserRequests;
+import redmine.model.Generatable;
+import redmine.utils.StringGenerators;
+
+/**
+ * Класс-модель пользователя в системе.
+ */
+@Data
+@Accessors(chain = true)
+public class User implements Generatable<User> {
+    private Integer id;
+    private String login = StringGenerators.randomEnglishLowerString(8);
+    private String password = StringGenerators.randomEnglishString(8);
+    private Boolean admin = false;
+    private Integer status = 1;
+    private String firstname = StringGenerators.randomEnglishString(8);
+    private String lastname = StringGenerators.randomEnglishString(8);
+    private String salt = StringGenerators.randomString(40, "0123456789abcdef");
+
+    // generateRandomString(40, "0123456789abcdef");
+
+    public String getApiKey() {
+        //TODO Изменить на генерацию ключа API
+        return "f02b2da01a468c4116be898911481d1b928c15f9";
+    }
+
+    @Override
+    public User read() {
+        return null;
+    }
+
+    @Override
+    public User update() {
+        return null;
+    }
+
+    @Override
+    public User create() {
+        return UserRequests.createUser(this);
+    }
+
+}
