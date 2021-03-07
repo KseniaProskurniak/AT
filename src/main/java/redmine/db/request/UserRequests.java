a98e16ccfbb379ae68ae0db56d634cc6e490e920
Ksenia_Proskurnyak <proskurnyak@i-teco.ru>
2021-03-07 23:58:05 +0300

    Реализовано удаление юзера через БД

--- src/main/java/redmine/db/request/UserRequests.java
+++ src/main/java/redmine/db/request/UserRequests.java
@@ -1,12 +1,74 @@
 package redmine.db.request;
 
+import lombok.SneakyThrows;
+import org.apache.commons.codec.digest.DigestUtils;
+import redmine.managers.Manager;
 import redmine.model.user.User;
 
-public class UserRequests {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
 
-    public static User createUser(User user){
-        //запрос в БД
-        //установка id значением, сгенерированным БД
+public class UserRequests {
+    @SneakyThrows
+    public static User createUser(User user) {
+        String query = "INSERT INTO public.users " +
+                "(id, login, hashed_password, firstname, lastname, admin, status, language, type, " +
+                "mail_notification, salt) " +
+                "VALUES(DEFAULT,?,?,?,?,?,?,?,?,?,?) RETURNING id;";
+        List<Map<String, Object>> result = Manager.dbConnection.executePreparedQuery(query,
+                user.getLogin(),
+                hashPassword(user.getSalt(), user.getPassword()),
+                user.getFirstname(),
+                user.getLastname(),
+                user.getAdmin(),
+                user.getStatus(),
+                "ru",
+                "User",
+                "all",
+                user.getSalt()
+        );
+        user.setId((Integer) result.get(0).get("id"));
+
+        query = "INSERT INTO public.tokens " +
+                "(id, user_id, action, value, created_on) " +
+                "VALUES(DEFAULT,?,'api',?,?) RETURNING id;";
+        PreparedStatement prepared = Manager.dbConnection.getConnection().prepareStatement(query);
+        prepared.setInt(1, user.getId());
+        prepared.setString(2, user.getApiKey());
+        prepared.setDate(3, new java.sql.Date(System.currentTimeMillis()));
+        prepared.executeQuery();
+
         return user;
+
+    }
+
+    @SneakyThrows
+    public static User findByLogin(String login) {
+        String query = "SELECT * FROM public.users WHERE login = ?;";
+        PreparedStatement prepared = Manager.dbConnection.getConnection().prepareStatement(query);
+        prepared.setString(1, login);
+        ResultSet result = prepared.executeQuery();
+        User user = new User();
+        while (result.next()) {
+            user.setId(result.getInt("id"));
+            user.setLogin(result.getString("login"));
+            user.setFirstname(result.getString("firstname"));
+            user.setLastname(result.getString("lastname"));
+        }
+        return user;
+    }
+
+    @SneakyThrows
+    public static void deleteUser(User user){
+        String query = "DELETE FROM public.users WHERE id = ?;";
+        PreparedStatement prepared = Manager.dbConnection.getConnection().prepareStatement(query);
+        prepared.setInt(1, user.getId());
+        prepared.executeUpdate();
+    }
+
+    private static String hashPassword(String salt, String plainPassword) {
+        return DigestUtils.sha1Hex(salt + DigestUtils.sha1Hex(plainPassword));
     }
 }
--- src/main/java/redmine/model/user/User.java
+++ src/main/java/redmine/model/user/User.java
@@ -1,35 +1,42 @@
 package redmine.model.user;
 
-import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.Accessors;
-import lombok.experimental.FieldDefaults;
 import redmine.db.request.UserRequests;
 import redmine.model.Generatable;
+import redmine.utils.StringGenerators;
 
+/**
+ * Класс-модель пользователя в системе.
+ */
 @Data
 @Accessors(chain = true)
-@FieldDefaults(level = AccessLevel.PRIVATE)
-
 public class User implements Generatable<User> {
-    private String login = "admin";
-    private String password = "admin123";
+    private Integer id;
+    private String login = StringGenerators.randomEnglishLowerString(8);
+    private String password = StringGenerators.randomEnglishString(8);
     private Boolean admin = false;
     private Integer status = 1;
-    // generateRandomString(40, "0123456789abcdef");
+    private String firstname = StringGenerators.randomEnglishString(8);
+    private String lastname = StringGenerators.randomEnglishString(8);
+    private String email = StringGenerators.randomEmail();
+    private String salt = StringGenerators.randomString(32, "0123456789abcdef");
+    private String apiKey = StringGenerators.randomString(40, "0123456789abcdef");
 
-    public String getApiKey() {
-        //TODO Изменить на генерацию ключа API
-        return "f02b2da01a468c4116be898911481d1b928c15f9";
-    }
+
+//    public String getApiKey() {
+//        return "f02b2da01a468c4116be898911481d1b928c15f9";
+//    }
 
     @Override
     public User read() {
+        //todo реализовать
         return null;
     }
 
     @Override
     public User update() {
+        //todo реализовать
         return null;
     }
 
@@ -39,8 +46,7 @@ public class User implements Generatable<User> {
     }
 
     @Override
-    public User delete() {
-        return null;
+    public void delete() {
+    UserRequests.deleteUser(this);
     }
-
 }
